950a2171be91e15ded85f82c60522c1241f49735
yeanjae0312 <yeanjae@naver.com>
2022-03-28 14:55:45 +0900

    Chore: vue 프로젝트 세팅

---
+++ mission/src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import App from './App.vue';
+
+library.add(fas, far);
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon)
+  .mount('#app');
