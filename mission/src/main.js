7d7eab2f30167a3278259da775c9159c15d21cd6
yeanjae0312 <yeanjae@naver.com>
2022-03-30 20:50:26 +0900

    Refactor: api 레퍼지토리패턴 변경으로 인한 main.js 수정

--- mission/src/main.js
+++ mission/src/main.js
@@ -3,16 +3,11 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-// import axios from 'axios';
 import router from './router';
 import store from './store';
 import App from './App.vue';
 
 library.add(fas, far);
 
-const app = createApp(App);
-
-// app.config.globalProperties.axios = axios;
-app.component('font-awesome-icon', FontAwesomeIcon).use(store).use(router).mount('#app');
-
-// axios.defaults.baseURL = 'https://ably-frontend-assignment-server.vercel.app';
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).use(router).use(store)
+  .mount('#app');
